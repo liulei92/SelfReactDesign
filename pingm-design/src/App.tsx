06edb36f3283031658d3ac62207f229e1d45764f
Lei Liu <leiliu@grandstream.cn>
2020-06-08 20:24:21 +0800

    添加 icon 组件
    Cause: None
    Solution: None
    Fixed Version: None
    Branch: None

--- pingm-design/src/App.tsx
+++ pingm-design/src/App.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+
 import Menu from './components/Menu/menu'
 import MenuItem from './components/Menu/menuItem'
 import SubMenu from './components/Menu/subMenu'
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+library.add(fas) // 引入所有图标
 
 function App () {
   return (
     <div className='App'>
-      <FontAwesomeIcon icon={faCoffee} size='2x' />
       <header className='App-header'>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
---
+++ pingm-design/src/components/Transition/transition.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+
+const Transition: React.FC = props => {
+  return (
+    <div></div>
+  )
+}
+
+export default Transition
